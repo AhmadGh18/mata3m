99cee2362d3a5f6256f3f7648b1ba747bdd1a652
AhmadGh18 <=>
2024-07-01 20:20:19 +0300

    Changing port Numer to 3000

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,53 +1,60 @@
-import React from 'react'
-import { FastFood, FoodBank, Location, User } from './icons'
+import React from "react";
+import { FastFood, FoodBank, Location, User } from "./icons";
 
 const Features = () => {
   return (
     <div>
-        <div className='w-[80%] m-auto p-2'>
-            <p className='font-bold text-xl'>Services</p>
-            <div className='flex items-center justify-center gap-10 p-4'>
-                <div className='flex items-center justify-center flex-wrap gap-10 pt-5'>
-                    <div className='w-36 text-center'>
-                    <div className='bg-orange w-16 h-16 text-3xl  rounded-full flex items-center justify-center m-auto'>
-                        <FoodBank/>
-                    </div>
-                    <p className='text-sm font-semibold'>Restaurant Profile</p>
-                    <p className='text-sm'>Easily create and manage restaurant profiles.</p>
-                    </div>           
-                </div>
-                <div className='flex items-center justify-center flex-wrap gap-10 pt-5'>
-                    <div className='w-36 text-center'>
-                    <div className='w-16 h-16 text-3xl bg-brownish rounded-full flex items-center justify-center m-auto'>
-                        <User/>
-                    </div>
-                    <p className='text-sm font-semibold'>Restaurant Profile</p>
-                    <p className='text-sm'>Easily create and manage restaurant profiles.</p>
-                    </div>           
-                </div>
-                <div className='flex items-center justify-center flex-wrap gap-10 pt-5'>
-                    <div className='w-36 text-center'>
-                    <div className='bg-gray w-16 h-16 text-3xl  rounded-full flex items-center justify-center m-auto'>
-                        <FastFood/>
-                    </div>
-                    <p className='text-sm font-semibold'>Restaurant Profile</p>
-                    <p className='text-sm'>Easily create and manage restaurant profiles.</p>
-                    </div>           
-                </div>
-                <div className='flex items-center justify-center flex-wrap gap-10 pt-5'>
-                    <div className='w-36 text-center'>
-                    <div className='bg-grayish w-16 h-16 text-3xl  rounded-full flex items-center justify-center m-auto'>
-                        <Location/>
-                    </div>
-                    <p className='text-sm font-semibold'>Restaurant Profile</p>
-                    <p className='text-sm'>Easily create and manage restaurant profiles.</p>
-                    </div>           
-                </div>
+      <div className="w-[80%] m-auto p-2">
+        <p className="font-bold text-xl">Services</p>
+        <div className="flex items-center justify-center gap-10 p-4">
+          <div className="flex items-center justify-center flex-wrap gap-10 pt-5">
+            <div className="w-36 text-center">
+              <div className="bg-orange w-16 h-16 text-3xl  rounded-full flex items-center justify-center m-auto">
+                <FoodBank />
+              </div>
+              <p className="text-sm font-semibold">Restaurant Profile</p>
+              <p className="text-sm">
+                Easily create and manage restaurant profiles.
+              </p>
             </div>
-            
+          </div>
+          <div className="flex items-center justify-center flex-wrap gap-10 pt-5">
+            <div className="w-36 text-center">
+              <div className="w-16 h-16 text-3xl bg-brownish rounded-full flex items-center justify-center m-auto">
+                <User />
+              </div>
+              <p className="text-sm font-semibold">Restaurant Profile</p>
+              <p className="text-sm">
+                Easily create and manage restaurant profiles.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center flex-wrap gap-10 pt-5">
+            <div className="w-36 text-center">
+              <div className="bg-gray w-16 h-16 text-3xl  rounded-full flex items-center justify-center m-auto">
+                <FastFood />
+              </div>
+              <p className="text-sm font-semibold">Restaurant Profile</p>
+              <p className="text-sm">
+                Easily create and manage restaurant profiles.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center flex-wrap gap-10 pt-5">
+            <div className="w-36 text-center">
+              <div className="bg-grayish w-16 h-16 text-3xl  rounded-full flex items-center justify-center m-auto">
+                <Location />
+              </div>
+              <p className="text-sm font-semibold">Restaurant Profile</p>
+              <p className="text-sm">
+                Easily create and manage restaurant profiles.
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
--- src/components/icons/index.jsx
+++ src/components/icons/index.jsx
@@ -1,9 +1,9 @@
 import { MdOutlineRestaurantMenu as Menu } from "react-icons/md";
-import { CiSearch as CSearch } from 'react-icons/ci'
+import { CiSearch as CSearch } from "react-icons/ci";
 import { MdDeliveryDining as Delivery } from "react-icons/md";
 import { MdFoodBank as FoodBank } from "react-icons/md";
-import { FaUser as User} from "react-icons/fa";
+import { FaUser as User } from "react-icons/fa";
 import { IoFastFood as FastFood } from "react-icons/io5";
 import { FaLocationDot as Location } from "react-icons/fa6";
 
-export { Menu, CSearch, Delivery, FoodBank, User, FastFood, Location }
+export { Menu, CSearch, Delivery, FoodBank, User, FastFood, Location };
